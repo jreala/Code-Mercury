891415e8c89288cfefa34bb957670a231890c859
Jason Eala <Jason Eala>
2016-12-08 14:50:42 -0800

    Menu scene plus UI buttons

---
+++ Project Mercury/Assets/Scripts/UI/UpdateUIButtons.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class UpdateUIButtons : MonoBehaviour
+{
+    public GameObject SwitchFrom;
+    public GameObject SwitchTo;
+
+    public void Switch()
+    {
+        SwitchFrom.SetActive(false);
+        SwitchTo.SetActive(true);
+    }
+}
